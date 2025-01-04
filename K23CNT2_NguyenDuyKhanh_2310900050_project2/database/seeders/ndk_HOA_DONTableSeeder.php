4a17faf96f45eaaa589988845f76cd1782c7c54a
Khánh Nguyễn <duykhanhduong088@gmail.com>
2025-01-04 08:44:21 +0700

    update

--- K23CNT2_NguyenDuyKhanh_2310900050_project2/database/seeders/ndk_HOA_DONTableSeeder.php
+++ K23CNT2_NguyenDuyKhanh_2310900050_project2/database/seeders/ndk_HOA_DONTableSeeder.php
@@ -4,33 +4,77 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
 
 class ndk_HOA_DONTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $faker = Faker::create(); // Khởi tạo Faker
+        DB::table('ndk_HOA_DON')->insert([
+            'ndkMaHoaDon' => 'HD001',
+            'ndkMaKhachHang' => 1, // Liên kết với khách hàng có id = 1
+            'ndkNgayHoaDon' => '2024-12-12',
+            'ndkNgayNhan' => '2024-12-13',
+            'ndkHoTenKhachHang' => 'Nguyễn Duy Khánh',
+            'ndkEmail' => 'duykhanh@gmail.com',
+            'ndkDienThoai' => '0345865380',
+            'ndkDiaChi' => 'Hạ Long-Quảng Ninh',
+            'ndkTongGiaTri' => '1650',
+            'ndkTrangThai' => 1,
+        ]);
 
-        // Tạo ra 10 hóa đơn ngẫu nhiên
-        for ($i = 0; $i < 10; $i++) {
-            DB::table('ndk_HOA_DON')->insert([
-                'ndkMaHoaDon' => 'HD' . str_pad($i + 1, 3, '0', STR_PAD_LEFT), // Tạo mã hóa đơn
-                'ndkMaKhachHang' => $faker->numberBetween(1, 10), // Khách hàng ngẫu nhiên từ 1 đến 10
-                'ndkNgayHoaDon' => $faker->date($format = 'Y/m/d', $max = 'now'), // Ngày hóa đơn
-                'ndkNgayNhan' => $faker->date($format = 'Y/m/d', $max = 'now'), // Ngày nhận hàng
-                'ndkHoTenKhachHang' => $faker->name, // Tên khách hàng ngẫu nhiên
-                'ndkEmail' => $faker->safeEmail, // Email ngẫu nhiên
-                'ndkDienThoai' => $faker->phoneNumber, // Số điện thoại ngẫu nhiên
-                'ndkDiaChi' => $faker->address, // Địa chỉ ngẫu nhiên
-                'ndkTongGiaTri' => $faker->numberBetween(100000, 1000000), // Tổng giá trị ngẫu nhiên
-                'ndkTrangThai' => $faker->numberBetween(0, 2), // Trạng thái hóa đơn ngẫu nhiên (0, 1, 2)
-            ]);
-        }
+        DB::table('ndk_HOA_DON')->insert([
+            'ndkMaHoaDon' => 'HD002',
+            'ndkMaKhachHang' => 2, // Liên kết với khách hàng có id = 2
+            'ndkNgayHoaDon' => '2024-12-20',
+            'ndkNgayNhan' => '2024-12-21',
+            'ndkHoTenKhachHang' => 'Đàm Tuấn Hưng',
+            'ndkEmail' => 'tuantran@gmail.com',
+            'ndkDienThoai' => '0123456788',
+            'ndkDiaChi' => 'Phú Thọ',
+            'ndkTongGiaTri' => '1650',
+            'ndkTrangThai' => 0,
+        ]);
+
+        DB::table('ndk_HOA_DON')->insert([
+            'ndkMaHoaDon' => 'HD003',
+            'ndkMaKhachHang' => 3, // Liên kết với khách hàng có id = 3
+            'ndkNgayHoaDon' => '2024-12-17',
+            'ndkNgayNhan' => '2024-12-18',
+            'ndkHoTenKhachHang' => 'Nguyễn Huy Thông',
+            'ndkEmail' => 'hthong@gmail.com',
+            'ndkDienThoai' => '0345678901',
+            'ndkDiaChi' => 'Uông Bí',
+            'ndkTongGiaTri' => '950',
+            'ndkTrangThai' => 1,
+        ]);
+
+        DB::table('ndk_HOA_DON')->insert([
+            'ndkMaHoaDon' => 'HD004',
+            'ndkMaKhachHang' => 4, // Liên kết với khách hàng có id = 4
+            'ndkNgayHoaDon' => '2024-12-03',
+            'ndkNgayNhan' => '2024-12-04',
+            'ndkHoTenKhachHang' => 'Phạm Tuấn Anh',
+            'ndkEmail' => 'phamtuan@gmail.com',
+            'ndkDienThoai' => '0133456789',
+            'ndkDiaChi' => 'Cẩm Phả',
+            'ndkTongGiaTri' => '1899',
+            'ndkTrangThai' => 1,
+        ]);
+
+        DB::table('ndk_HOA_DON')->insert([
+            'ndkMaHoaDon' => 'HD005',
+            'ndkMaKhachHang' => 1, // Liên kết lại với khách hàng có id = 1
+            'ndkNgayHoaDon' => '2024-12-25',
+            'ndkNgayNhan' => '2024-12-26',
+            'ndkHoTenKhachHang' => 'Nguyễn Duy Khánh',
+            'ndkEmail' => 'duykhanh@gmail.com',
+            'ndkDienThoai' => '0345865380',
+            'ndkDiaChi' => 'Hạ Long-Quảng Ninh',
+            'ndkTongGiaTri' => '1750',
+            'ndkTrangThai' => 2,
+        ]);
     }
 }
--- K23CNT2_NguyenDuyKhanh_2310900050_project2/database/seeders/ndk_KHACH_HANGTableSeeder.php
+++ K23CNT2_NguyenDuyKhanh_2310900050_project2/database/seeders/ndk_KHACH_HANGTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash; // Đảm bảo Hash được sử dụng
 
 class ndk_KHACH_HANGTableSeeder extends Seeder
 {
@@ -13,20 +13,49 @@ class ndk_KHACH_HANGTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create(); // Khởi tạo Faker
+        DB::table('ndk_KHACH_HANG')->insert([
+            'ndkMaKhachHang' => 'KH005', // Thay đổi mã khách hàng
+            'ndkHoTenKhachHang' => 'Nguyễn Duy Khánh',
+            'ndkEmail' => 'duykhanh@gmail.com',
+            'ndkMatKhau' => Hash::make('123456a@'), // Mã hóa mật khẩu
+            'ndkDienThoai' => '0345865380',
+            'ndkDiaChi' => 'Hạ Long-Quảng Ninh',
+            'ndkNgayDangKy' => '2024-01-13',
+            'ndkTrangThai' => 0,
+        ]);        
 
-        // Tạo 10 khách hàng ngẫu nhiên
-        foreach (range(1, 10) as $index) {
-            DB::table('ndk_KHACH_HANG')->insert([
-                'ndkMaKhachHang' => 'KH' . str_pad($index, 3, '0', STR_PAD_LEFT),
-                'ndkHoTenKhachHang' => $faker->name, // Tên khách hàng
-                'ndkEmail' => $faker->email, // Email ngẫu nhiên
-                'ndkMatKhau' => bcrypt('123456a@'), // Mật khẩu mặc định
-                'ndkDienThoai' => $faker->phoneNumber, // Số điện thoại ngẫu nhiên
-                'ndkDiaChi' => $faker->address, // Địa chỉ ngẫu nhiên
-                'ndkNgayDangKy' => $faker->date('Y-m-d', 'now'), // Ngày đăng ký ngẫu nhiên
-                'ndkTrangThai' => rand(0, 2), // Trạng thái ngẫu nhiên (0, 1, hoặc 2)
-            ]);
-        }
+        DB::table('ndk_KHACH_HANG')->insert([
+            'ndkMaKhachHang' => 'KH002',
+            'ndkHoTenKhachHang' => 'Đàm Tuấn Hưng',
+            'ndkEmail' => 'tuantran@gmail.com',
+            'ndkMatKhau' => Hash::make('123456a@'), // Mã hóa mật khẩu
+            'ndkDienThoai' => '0123456788',
+            'ndkDiaChi' => 'Phú Thọ',
+            'ndkNgayDangKy' => '2024-12-20', // Thay đổi định dạng ngày
+            'ndkTrangThai' => 0,
+        ]);
+
+        DB::table('ndk_KHACH_HANG')->insert([
+            'ndkMaKhachHang' => 'KH003',
+            'ndkHoTenKhachHang' => 'Nguyễn Huy Thông',
+            'ndkEmail' => 'hthong@gmail.com',
+            'ndkMatKhau' => Hash::make('123456a@'), // Mã hóa mật khẩu
+            'ndkDienThoai' => '0345678901', // Thay đổi số điện thoại
+            'ndkDiaChi' => 'Uông Bí',
+            'ndkNgayDangKy' => '2024-12-13', // Định dạng ngày chuẩn
+            'ndkTrangThai' => 0, // Trạng thái có thể thay đổi tuỳ yêu cầu
+        ]);
+        
+
+        DB::table('ndk_KHACH_HANG')->insert([
+            'ndkMaKhachHang' => 'KH004',
+            'ndkHoTenKhachHang' => 'Phạm Tuấn Anh',
+            'ndkEmail' => 'phamtuan@gmail.com',
+            'ndkMatKhau' => Hash::make('123456a@'), // Mã hóa mật khẩu
+            'ndkDienThoai' => '0133456789',
+            'ndkDiaChi' => 'Cẩm Phả',
+            'ndkNgayDangKy' => '2024-12-03', // Thay đổi định dạng ngày
+            'ndkTrangThai' => 0,
+        ]);
     }
 }
